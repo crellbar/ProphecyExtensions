2aab86f04172b167df079dc9a3dced3630685d9e
Paul Crellin <crellbar@crellbar.com>
2018-03-24 18:19:16 +0000

    Add the fluid promise

---
+++ src/ProphecyExtensions/FluidPromise.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * (c) Paul Crellin <development@crellbar.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Crellbar\ProphecyExtensions;
+
+use Prophecy\Promise\PromiseInterface;
+use Prophecy\Prophecy\ObjectProphecy;
+use Prophecy\Prophecy\MethodProphecy;
+
+class FluidPromise implements PromiseInterface
+{
+    /**
+     * Returns Object
+     *
+     * @param array          $args
+     * @param ObjectProphecy $object
+     * @param MethodProphecy $method
+     *
+     * @return mixed
+     */
+    public function execute(array $args, ObjectProphecy $object, MethodProphecy $method)
+    {
+        return $object->reveal();
+    }
+}
